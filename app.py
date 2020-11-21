1982a49235ff2301ea699ee1511b7f0115060acb
tommin <tomas.mineika@outlook.com>
2020-11-21 22:30:40 +0200

    Main functionality completed

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, json, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5433/todo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Task(db.Model):
+class JsonModel(object):
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+class Task(db.Model, JsonModel):
     __tablename__ = 'task'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
@@ -21,17 +27,35 @@ class Task(db.Model):
 
 @app.route('/add', methods=['POST'])
 def submit():
-    if request.method == 'POST':
-        name = request.form['name']
-        description = request.form['description']
-        if name == '':
-            return 'Name cannot be empty'
-        if db.session.query(Task).filter(Task.name == name).count() == 0:
-            data = Task(name, description)
-            db.session.add(data)
-            db.session.commit()
-            return 'Success'
-        return 'Task with the same name already exists'
+    name = request.json.get('name')
+    description = request.json.get('description')
+    if name == '':
+        return jsonify(name ='Name cannot be empty'), 400
+    if len(name)>100:
+        return jsonify(name ='Name is too long'), 400
+    if db.session.query(Task).filter(Task.name == name).count() == 0:
+        data = Task(name, description)
+        db.session.add(data)
+        db.session.commit()
+        return json.dumps([u.as_dict() for u in Task.query.all()])
+    return jsonify(name ='Task with the same name already exists'), 400
+
+@app.route('/tasks', methods = ['GET'])
+def index():
+    return json.dumps([u.as_dict() for u in Task.query.all()])
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = Task.query.filter_by(id=id).first()
+    task.is_done = not task.is_done
+    db.session.commit()
+    return json.dumps([u.as_dict() for u in Task.query.all()])
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    Task.query.filter_by(id=id).delete()
+    db.session.commit()
+    return json.dumps([u.as_dict() for u in Task.query.all()])
 
 if __name__ == '__main__':
     app.debug = True
